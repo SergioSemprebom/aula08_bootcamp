1118b99e55a180917d243436b002e5757dbde6ed
Sergio Semprebom <sergiosemprebom@gmail.com>
2024-09-04 23:12:42 -0300

    salavando o projeto

--- etl.py
+++ etl.py
@@ -16,16 +16,21 @@ def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
     df['Total'] = df["Quantidade"] * df["Venda"]
     return df
 
-
 # uma funcao que da load em csv ou parquet
 
-# DICA DE MILHOES -> se quiser ficar testanto as FUNCTIONS
-# Funçao extrair_dados
-'''if __name__ == "__main__":
-    pasta = "pasta"
-    print(extrair_dados(path=pasta))'''
+def carregar_dados(df: pd.DataFrame, format_saida: list):
+    """
+    parametro que vai ser ou "csv" ou "parquet" ou "os dois"
+    """
+    for formato in format_saida:
+        if formato == "csv":
+            df.to_csv("dados.csv", index=False)
+        if formato == "parquet":
+            df.to_parquet("dados.parquet", index=False)
 
-if __name__ == "__main__":
-    pasta_argumento = "data"
-    data_frame = extrair_dados_e_consolidar(path=pasta_argumento)
-    print(calcular_kpi_de_total_de_vendas(data_frame))
+def pipeline_calcular_kpi_de_vendas_consolidado(pasta: str, formato_de_saida: list):
+    data_frame = extrair_dados_e_consolidar(pasta)
+    data_frame_calculando = calcular_kpi_de_total_de_vendas(data_frame)
+    formato_de_saida: list = ["csv", "parquet"]
+    carregar_dados(data_frame_calculando, formato_de_saida)
+    
---
+++ test_etl.py
@@ -0,0 +1,14 @@
+# TESTE PARA BASE DO MEU CODIGO PRINCIPAL etl
+
+# DICA DE MILHOES -> se quiser ficar testanto as FUNCTIONS
+'''
+if __name__ == "__main__":
+    pasta = "pasta"
+    print(extrair_dados_e_consolidar(path=pasta))
+
+
+
+    if __name__ == "__main__":
+    pasta_argumento = "data"
+    data_frame = extrair_dados_e_consolidar(pasta=pasta_argumento)
+    print(calcular_kpi_de_total_de_vendas(data_frame))'''
